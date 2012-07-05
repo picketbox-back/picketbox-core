20ebf7ac9e99ffd386db0830e9c7aa6a5b2cca1e
Anil Saldhana <Anil.Saldhana@jboss.com>
2012-07-05 16:13:53 -0500

    get the first cut at htpt basic impl

---
+++ picketbox-core/src/main/java/org/picketbox/authentication/http/HTTPBasicAuthentication.java
@@ -0,0 +1,108 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2012, Red Hat, Inc., and individual contributors
+ * as indicated by the @author tags. See the copyright.txt file in the
+ * distribution for a full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package org.picketbox.authentication.http;
+
+import java.io.IOException;
+import java.security.Principal;
+
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.picketbox.authentication.AuthenticationManager;
+import org.picketbox.authentication.PicketBoxConstants;
+import org.picketbox.exceptions.AuthenticationException;
+import org.picketbox.util.Base64;
+
+/**
+ * Perform HTTP Basic Authentication
+ * @author anil saldhana
+ */
+public class HTTPBasicAuthentication {
+
+    protected AuthenticationManager authManager;
+
+    protected String realmName = "PicketBox Realm";
+
+    public AuthenticationManager getAuthManager() {
+        return authManager;
+    }
+
+    public void setAuthManager(AuthenticationManager authManager) {
+        this.authManager = authManager;
+    }
+
+    public String getRealmName() {
+        return realmName;
+    }
+
+    public void setRealmName(String realmName) {
+        this.realmName = realmName;
+    }
+
+    public boolean authenticate(ServletRequest servletReq, ServletResponse servletResp) throws AuthenticationException {
+        String username, password;
+
+        HttpServletRequest request = (HttpServletRequest) servletReq;
+        HttpServletResponse response = (HttpServletResponse) servletResp;
+        // Get the Authorization Header
+        String authorizationHeader = request.getHeader(PicketBoxConstants.HTTP_AUTHORIZATION_HEADER);
+
+        if (authorizationHeader != null && authorizationHeader.isEmpty() == false) {
+
+            int whitespaceIndex = authorizationHeader.indexOf(' ');
+
+            if (whitespaceIndex > 0) {
+                String method = authorizationHeader.substring(0, whitespaceIndex);
+                
+                if (PicketBoxConstants.HTTP_BASIC.equalsIgnoreCase(method)) {
+                    authorizationHeader = authorizationHeader.substring(whitespaceIndex + 1);
+                    authorizationHeader = new String(Base64.decode(authorizationHeader));
+                    int indexOfColon = authorizationHeader.indexOf(':');
+                    if (indexOfColon > 0) {
+                        username = authorizationHeader.substring(0, indexOfColon);
+                        password = authorizationHeader.substring(indexOfColon + 1);
+
+                        if(authManager == null){
+                            throw new AuthenticationException("Auth Manager is not injected");
+                        }
+                        
+                        Principal principal = authManager.authenticate(username, password);
+                        if (principal != null) {
+                            request.setAttribute(PicketBoxConstants.PRINCIPAL, principal);
+                            return true;
+                        }
+                    }
+                }
+            }
+        }
+
+        response.setHeader(PicketBoxConstants.HTTP_WWW_AUTHENTICATE, "basic realm=\"" + realmName + '"');
+        try {
+            response.sendError(HttpServletResponse.SC_UNAUTHORIZED);
+        } catch (IOException e) {
+            throw new AuthenticationException(e);
+        }
+        return false;
+    }
+}
---
+++ picketbox-core/src/test/java/org/picketbox/test/authentication/http/HTTPBasicAuthenticationTestCase.java
@@ -0,0 +1,118 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2012, Red Hat, Inc., and individual contributors
+ * as indicated by the @author tags. See the copyright.txt file in the
+ * distribution for a full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package org.picketbox.test.authentication.http;
+
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.security.Principal;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.picketbox.authentication.AuthenticationManager;
+import org.picketbox.authentication.PicketBoxConstants;
+import org.picketbox.authentication.http.HTTPBasicAuthentication;
+import org.picketbox.exceptions.AuthenticationException;
+import org.picketbox.test.http.TestServletRequest;
+import org.picketbox.test.http.TestServletResponse;
+import org.picketbox.util.Base64;
+
+/**
+ * Unit test the {@link HTTPBasicAuthentication} class
+ * @author anil saldhana
+ */
+public class HTTPBasicAuthenticationTestCase {
+
+    private HTTPBasicAuthentication httpBasic = null;
+
+    @Before
+    public void setup() throws Exception{
+        httpBasic = new HTTPBasicAuthentication();
+        
+        httpBasic.setAuthManager(new AuthenticationManager() {
+            
+            @Override
+            public Principal authenticate(final String username, Object credential) throws AuthenticationException {
+                if("Aladdin".equalsIgnoreCase(username) && "Open Sesame".equalsIgnoreCase((String) credential)){
+                    return new Principal() {
+                        @Override
+                        public String getName() {
+                            return username;
+                        }
+                    };
+                }
+                return null;
+            }
+        });
+    }
+
+    @Test
+    public void testHttpBasic() throws Exception{
+        TestServletRequest req = new TestServletRequest(new InputStream(){
+            @Override
+            public int read() throws IOException {
+                return 0;
+            }});
+
+        TestServletResponse resp = new TestServletResponse(new OutputStream(){
+
+            @Override
+            public void write(int b) throws IOException {
+                System.out.println(b);
+            }});
+
+        //Get Positive Authentication
+        req.addHeader(PicketBoxConstants.HTTP_AUTHORIZATION_HEADER, "Basic " + getPositive());
+        boolean result = httpBasic.authenticate(req, resp);
+        
+        assertTrue(result);
+        
+        req.clearHeaders();
+        
+        //Get Negative Authentication
+        req.addHeader(PicketBoxConstants.HTTP_AUTHORIZATION_HEADER, "Basic " + getNegative());
+        result = httpBasic.authenticate(req, resp);
+        assertFalse(result);
+        
+        String basicHeader = resp.getHeader(PicketBoxConstants.HTTP_WWW_AUTHENTICATE);
+        assertTrue(basicHeader.startsWith("basic realm="));
+    }
+    
+    private String getPositive(){
+        String str =  "Aladdin:open sesame";
+        String encoded = Base64.encodeBytes(str.getBytes());
+        assertEquals("QWxhZGRpbjpvcGVuIHNlc2FtZQ==", encoded);
+        return encoded;
+    }
+    
+
+    private String getNegative(){
+        String str =  "Aladdin:Bad sesame";
+        String encoded = Base64.encodeBytes(str.getBytes());
+        return encoded;
+    }
+}
