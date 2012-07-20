986b9cf420d3e8e5d903bb6a8f148cb9f43bb37a
Anil Saldhana <Anil.Saldhana@jboss.com>
2012-07-20 14:11:35 -0500

    Bring in embedded apacheds

---
+++ picketbox-core/src/test/java/org/picketbox/test/ldap/apacheds/AbstractLDAPTest.java
@@ -0,0 +1,84 @@
+/*
+ * JBoss, Home of Professional Open Source.
+ * Copyright 2012, Red Hat, Inc., and individual contributors
+ * as indicated by the @author tags. See the copyright.txt file in the
+ * distribution for a full listing of individual contributors.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this software; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+ */
+package org.picketbox.test.ldap.apacheds;
+
+import static org.junit.Assert.assertNotNull;
+
+import java.io.File;
+import java.io.InputStream;
+
+import org.junit.After;
+import org.junit.Before;
+
+/**
+ * Base class for ldap tests
+ * @author anil saldhana
+ * @since Jul 20, 2012
+ */
+public class AbstractLDAPTest {
+    protected String adminPW = "secret";
+    protected String dn = "dc=jboss,dc=org";
+    protected String adminDN = "uid=admin,ou=system";
+    protected String port = "10389";
+    
+    protected String serverHost = "localhost";
+ 
+    protected EmbeddedApacheDS ds =null;
+    
+    @Before
+    public void setup() throws Exception {
+        File workDir = new File( System.getProperty( "java.io.tmpdir" ) + "/server-work" );
+        workDir.mkdirs();
+        
+        ds = new EmbeddedApacheDS(workDir);
+        ds.createBaseDN("apache", "dc=apache,dc=org");
+        
+        ds.createBaseDN("jboss", "dc=jboss,dc=org");
+    
+        long current = System.currentTimeMillis();
+        System.out.println("Starting Apache DS server");
+        ds.startServer();
+
+        System.out.println("Time taken = " + (System.currentTimeMillis() - current ) + "milisec");
+        
+        InputStream is = getClass().getClassLoader().getResourceAsStream("ldap/users.ldif");
+        assertNotNull(is);
+        ds.importLdif(is);
+    }
+    
+    @After
+    public void tearDown() throws Exception
+    {
+        if(ds != null){
+            ds.stopServer();
+        }
+    }
+    
+    protected void importLDIF(String fileName) throws Exception{
+        long current = System.currentTimeMillis();
+        System.out.println("Going to import LDIF:" + fileName);
+        InputStream is = getClass().getClassLoader().getResourceAsStream(fileName); 
+        assertNotNull(is);
+        ds.importLdif(is);
+        System.out.println("Time taken = " + (System.currentTimeMillis() - current ) + "milisec");
+    }
+}
